fc868f9222607b2a8041e438221d62bbe709e1b1
Peter Ekwere <105059347+PeterEkwere@users.noreply.github.com>
2025-02-17 08:53:39 +0000

    Added new Functionality to api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,20 +1,20 @@
 export const notifyNewUser = async () => {
-    try {
-      const response = await fetch('http://18.144.169.247:5000/notify', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({}),
-      });
-  
-      if (!response.ok) {
-        throw new Error('Failed to notify');
-      }
-  
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error notifying new user:', error);
+  try {
+    const response = await fetch('http://18.144.169.247:5000/notify', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({}),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to notify');
     }
-  };
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error notifying new user:', error);
+  }
+};
