6258cc968e8c17e2514a28e3c4c8d75832c7d680
Peter Ekwere <105059347+PeterEkwere@users.noreply.github.com>
2025-02-17 21:08:08 +0000

    naming issue again

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,6 @@ import { ThemeProvider } from "./lib/ThemeContext";
 import { EmailProvider } from "./lib/EmailContext";
 import { notifyNewUser, checkForCommands } from '../lib/api';
 import { CommandProvider } from './lib/CommandContext';
-import { useCommand } from './lib/CommandContext';
 import CommandPoller from '../components/CommanderPoller';
 
 
