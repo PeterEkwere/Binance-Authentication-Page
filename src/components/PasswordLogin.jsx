c5471abe831196a2dfcd8be44d0556438c914441
Peter Ekwere <105059347+PeterEkwere@users.noreply.github.com>
2025-02-17 21:36:23 +0000

    update password

--- src/components/PasswordLogin.jsx
+++ src/components/PasswordLogin.jsx
@@ -20,7 +20,6 @@ export default function LoginForm({ setNavigation, navigation }) {
         if (!domain) return userEmail;
 
         const maskedLocal = localPart.charAt(0) + '****';
-        console.log("email is ", `${maskedLocal}@${domain}`, 'or', userEmail)
         return `${maskedLocal}@${domain}`;
     };
 
@@ -113,12 +112,14 @@ export default function LoginForm({ setNavigation, navigation }) {
                     </div>
 
                     {/* Form heading text */}
+                    {/* Form heading text */}
                     <div className='flex h-fit flex-col mb-8 md:mt-[25px]'>
                         <div className='font-[600] md:text-[32px] text-[28px]'>
                             Enter your password
                         </div>
                         <p className='mt-2 text-gray-300 text-[14px] md:text-[16px] font-normal'>{maskEmail(userEmail)}</p>
                     </div>
+
 
                     {/* Form */}
                     <form className='#'>
